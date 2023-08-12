843ca01f3c9d764f0d47c264dcfbcfaa4a25b333
adeeltalib <adeeltalib6270@gmail.com>
2023-08-12 15:15:14 +0500

    pages created

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,32 +1,59 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, {useEffect} from "react";
 import { useRouter } from "next/navigation";
-// import {axios} from "axios";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+
 
 
 
 
 
 const Signup = () => {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email:"",
     password: "",
     username: "",    
   })
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+  const [loading, setLoading] = React.useState(false);
 
   const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success, response.data");
+      router.push("/login");
 
+
+    } catch (error: any) {
+      console.log("Signup failed", error.message )
+      toast.error(error.message)
+    }finally {
+      setLoading(false);
+    }
   }
+
+useEffect(()=> {
+if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+  setButtonDisabled(false);
+} else {
+  setButtonDisabled(true)
+}
+}, [user]);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-center text-black text-2xl my-12">
-      Signup
+      {loading ? "Processing" : "Signup"}
         </h1>
         <hr />
         <label htmlFor="username">username</label>
         <input
-        className="my-2 p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+        className="my-2 p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
        id="username"
        type="text"
        value={user.username}
@@ -36,7 +63,7 @@ const Signup = () => {
        
        <label htmlFor="email ">Email</label>
         <input
-        className="my-2 p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="my-2 p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
        id="email"
        type="text"
        value={user.email}
@@ -45,7 +72,7 @@ const Signup = () => {
        />
        <label htmlFor="username">Password</label>
         <input
-        className="my-2 p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="my-2 p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
        id="password"
        type="password"
        value={user.password}
@@ -54,7 +81,9 @@ const Signup = () => {
        />
        <button 
        onClick={onSignup}
-       className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Signup Now</button>
+       className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black">
+        {buttonDisabled ? "No Signup" : "Signup"}
+        </button>
         
         <Link href="/login">Login Here</Link>
         </div>
